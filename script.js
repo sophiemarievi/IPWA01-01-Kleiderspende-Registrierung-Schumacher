0b38c57a755db0c519389ede743d915b36895dbe
sophiemarievi <118815569+sophiemarievi@users.noreply.github.com>
2023-01-10 10:48:24 +0100

    Funktion Plz übereprüfen

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 function validPlz() {
-	let plz = document.getElementById("form2DonationPlz");
-	if (plz < 50000 || plz > 50999) {
+	let plz = document.getElementById("form2DonationPlz").value;
+	if (plz.length < 5) {
+		alert("Deine Postleizahl ist zu kurz");
+	} else if (plz.length > 5) {
+		alert("Deine Postleizahl ist zu lang");
+	} else if (plz < 50000 || plz > 50999) {
 		alert("Leider fahren wir nicht in dein Gebiet.");
 	}
 }
 
-let plz = document.getElementById("form2DonationPlz");
-plz.onblur = validPlz;
